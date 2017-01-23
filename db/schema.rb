9ea467c80a2ead774039b7948aae6a8f319a7a37
Pedro Sánchez <pedrozo_96@hotmail.com>
2017-01-23 16:03:38 -0400

    corrigiendo el error de no routes matches con reviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170123135455) do
+ActiveRecord::Schema.define(version: 20170123200217) do
 
   create_table "animes", force: :cascade do |t|
     t.string   "title"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20170123135455) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "anime_id"
   end
 
   create_table "users", force: :cascade do |t|
