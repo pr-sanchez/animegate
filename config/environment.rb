8bb4663eb98bc6b645d3c785c35a625e32063c27
Pedro Sánchez <pedrozo_96@hotmail.com>
2017-04-29 18:13:44 -0400

    Tratando de arreglar problemas con mailer en heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-config.action_mailer.default_url_options = { :host => 'animegate.herokuapp.com' }
+config.action_mailer.default_url_options = { :host => 'animegate.heroku.com' }
